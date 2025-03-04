dc722207e1470e91ab992eb93fea71a02b23a2e9
Santy86-arq <sjvx86@gmail.com>
2025-03-04 17:44:55 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript para interactividad básica
+document.getElementById('formulario-contacto').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Gracias por contactarnos. Nos pondremos en contacto contigo pronto.');
+});
